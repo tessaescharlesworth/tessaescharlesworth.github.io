6ef4e69213273ad06d6488560afb0db9102d1c2d
tessaescharlesworth <42980715+tessaescharlesworth@users.noreply.github.com>
2018-09-22 13:09:47 -0500

    Update Trans.php

--- Trans.php
+++ Trans.php
@@ -7,19 +7,19 @@ $q3 = $_POST['Demographic'];
 
 
 if ($q3 == "Gender") {
-    include 'Gender.php';
+    include 'tessaescharlesworth.github.io/Gender.php';
 
 } elseif ($q3 == "Age") {
-    include 'Age.php';
+    include 'tessaescharlesworth.github.io/Age.php';
 }
   elseif ($q3 == "Education") {
-     include 'Education.php';
+     include 'tessaescharlesworth.github.io/Education.php';
 }
   elseif ($q3 == "Political_Ideology") {
-     include 'Political_Ideology.php';
+     include 'tessaescharlesworth.github.io/Political_Ideology.php';
 }
   else {
-     include 'Race.php';
+     include 'tessaescharlesworth.github.io/Race.php';
      
     
 }
